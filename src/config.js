228b8237763db40a5b77d72ba05932826837fd94
Mamba <dengzhifeng_63@163.com>
2020-10-19 19:04:38 +0800

    commit message

---
+++ src/config.js
@@ -0,0 +1,24 @@
+/* ========================================================
+                        小程序配置文件
+======================================================== */
+// 域名
+if (_NODE_) {
+  var host = 'https://love.ufutx.com/api' // 测试环境
+} else {
+  // var host = 'http://alpha.ufutx.net/api'  // 线上环境
+  var host = 'https://love.ufutx.com/api' // 测试环境
+  // var host = 'http://love.hankin.cn/api' // 测试环境
+}
+
+export const service = {
+  // 登录接口
+  login: `${host}/login/wechat`,
+  // 微信注册接口
+  register: `${host}/wechat/register`,
+  // 主域
+  host: `${host}`
+}
+
+export default {
+  service
+}
